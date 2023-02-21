ab433e0fb26fb9117c10da4d2ac3c34804eaec8b
williamrx777 <williamr723@gmail.com>
2023-02-20 21:51:34 -0300

    atualizando mensagem urls views e templates para chat

--- mensagem/urls.py
+++ mensagem/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('enviar_mensagem/<int:destinatario_id>/', views.enviar_mensagem, name='enviar_mensagem'),
-    path('conversa/<int:destinatario_id>/', views.exibir_conversa, name='exibir_conversa'),
+    path('chat/', views.chat, name='chat'),
+    path('chat/<int:destinatario_id>/', views.chat, name='chat'),
+
 ]
--- mensagem/views.py
+++ mensagem/views.py
@@ -4,21 +4,25 @@ from django.contrib.auth.decorators import login_required
 from .models import Mensagem
 
 @login_required
-def enviar_mensagem(request, destinatario_id):
-    destinatario = get_object_or_404(User, id=destinatario_id)
-    if request.user == destinatario:
-        return redirect('perfil')
-    # verificar se o destinatário é um amigo do usuário, etc.
-    if request.method == 'POST':
-        texto = request.POST.get('texto')
-        mensagem = Mensagem(remetente=request.user, destinatario=destinatario, texto=texto)
-        mensagem.save()
+def chat(request, destinatario_id=None):
+    destinatario = None
+    mensagens = None
 
-    return render(request, 'enviar_mensagem.html', {'destinatario': destinatario})
+    if destinatario_id:
+        destinatario = get_object_or_404(User, id=destinatario_id)
 
-@login_required
-def exibir_conversa(request, destinatario_id):
-    destinatario = get_object_or_404(User, id=destinatario_id)
-    mensagens = Mensagem.objects.filter(remetente=request.user, destinatario=destinatario) | Mensagem.objects.filter(remetente=destinatario, destinatario=request.user)
-    mensagens = mensagens.order_by('data_hora')
-    return render(request, 'exibir_conversa.html', {'destinatario': destinatario, 'mensagens': mensagens})
+        if request.user == destinatario:
+            return redirect('perfil')
+
+        if request.method == 'POST':
+            texto = request.POST.get('texto')
+            mensagem = Mensagem(remetente=request.user, destinatario=destinatario, texto=texto)
+            mensagem.save()
+
+            # Redireciona para a mesma URL, mas sem o parâmetro do destinatário
+            return redirect('chat')
+
+        mensagens = Mensagem.objects.filter(remetente=request.user, destinatario=destinatario) | Mensagem.objects.filter(remetente=destinatario, destinatario=request.user)
+        mensagens = mensagens.order_by('data_hora')
+
+    return render(request, 'chat.html', {'destinatario': destinatario, 'mensagens': mensagens})
